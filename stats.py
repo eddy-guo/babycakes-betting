761fa73f5b271ade5bff4bca11796466dcfd6f0c
eddy-guo <eddyyanruguo@gmail.com>
2023-05-09 10:38:07 -0400

    naming convention, remove career, relevant stats+average+specific values

--- stats.py
+++ stats.py
@@ -1,7 +1,8 @@
-from nba_api.stats.endpoints import playercareerstats, playergamelog
+from nba_api.stats.endpoints import playergamelog
 from nba_api.stats.static import players
+import pandas as pd
 
-player = players.get_active_players()
+player = players.get_players()
 
 jokic = [x for x in player if x["full_name"] == "Nikola Jokic"][0]
 murray = [x for x in player if x["full_name"] == "Jamal Murray"][0]
@@ -9,10 +10,14 @@ murray = [x for x in player if x["full_name"] == "Jamal Murray"][0]
 def createGamelog(id, season, type):
     return playergamelog.PlayerGameLog(player_id=id, season=season, season_type_all_star=type)
 
-jokicRegularSeason = createGamelog(jokic["id"], ["2022", "2023"], "Regular Season")
-jokicPlayoffs = createGamelog(jokic["id"], ["2022", "2023"], "Playoffs")
-murrayRegularSeason = createGamelog(murray["id"], ["2022", "2023"], "Regular Season")
-murrayPlayoffs = createGamelog(murray["id"], ["2022", "2023"], "Playoffs")
+jokic_rs = pd.concat(createGamelog(jokic["id"], ["2022", "2023"], "Regular Season").get_data_frames())
+jokic_po = pd.concat(createGamelog(jokic["id"], ["2022", "2023"], "Playoffs").get_data_frames())
 
-jokicCareer = playercareerstats.PlayerCareerStats(player_id=jokic["id"])
-murrayCareer = playercareerstats.PlayerCareerStats(player_id=murray["id"])
+jokic_rs_stats = pd.DataFrame(data=jokic_rs, columns=("MIN", "PTS", "REB", "AST", "STL", "BLK"))
+jokic_po_stats = pd.DataFrame(data=jokic_po, columns=("MIN", "PTS", "REB", "AST", "STL", "BLK"))
+
+jokic_rs_average = jokic_rs_stats.mean(axis="index")
+jokic_po_average = jokic_po_stats.mean(axis="index")
+
+jokic_rs_over30 = jokic_rs_stats[jokic_rs_stats["PTS"] >= 30]
+jokic_po_over30 = jokic_po_stats[jokic_po_stats["PTS"] >= 30]
